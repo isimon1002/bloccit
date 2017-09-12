d10708a54a4a174cbcd51c850adf9a8b070163fa
Isaac Simon <ijsimon@sbcglobal.net>
2017-09-12 22:20:22 +0000

    checkpoint31

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,6 +7,8 @@ class Post < ApplicationRecord
     
     default_scope { order('rank DESC') }
     
+    scope :visible_to, -> (user) { user ? all : joins(:topic).where('topics.public' => true) }
+    
     validates :title, length: { minimum: 5 }, presence: true
     validates :body, length: { minimum: 20 }, presence: true
     validates :topic, presence: true
@@ -32,5 +34,5 @@ class Post < ApplicationRecord
      age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
      new_rank = points + age_in_days
      update_attribute(:rank, new_rank)
-   end
+    end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,12 +5,9 @@
    let(:description) { RandomData.random_paragraph }
    let(:title) { RandomData.random_sentence }
    let(:body) { RandomData.random_paragraph }
- # #3
-   let(:topic) { Topic.create!(name: name, description: description) }
- # #4
-   let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "helloworld") }
- # #2
-   let(:post) { topic.posts.create!(title: title, body: body, user: user) }
+   let(:topic) { create(:topic) }
+   let(:user) { create(:user) }
+   let(:post) { create(:post) }
  
    it { is_expected.to belong_to(:topic) }
    it { is_expected.to belong_to(:user) }
@@ -27,8 +24,8 @@
    it { is_expected.to validate_length_of(:body).is_at_least(20) }
 
    describe "attributes" do
-     it "has a title body and user attribute" do
-       expect(post).to have_attributes(title: title, body: body, user: user)
+     it "has a title and body attribute" do
+       expect(post).to have_attributes(title: post.title, body: post.body)
      end
    end
    
