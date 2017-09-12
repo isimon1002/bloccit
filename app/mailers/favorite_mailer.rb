a4fc52a9552f557f8709ec734e3f1faaead4f275
Isaac Simon <ijsimon@sbcglobal.net>
2017-09-12 21:15:38 +0000

    checkpoint 30 Assignment

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -15,4 +15,15 @@ class FavoriteMailer < ApplicationMailer
  # #19
      mail(to: user.email, subject: "New comment on #{post.title}")
    end
+   
+   def new_post(post, user)
+     headers["Message-ID"] = "<post/#{post.id}@http://bloccit5-isimon1002.c9users.io:8080/>"
+     headers["In-Reply-To"] = "<post/#{post.id}@http://bloccit5-isimon1002.c9users.io:8080/>"
+     headers["References"] = "<post/#{post.id}@http://bloccit5-isimon1002.c9users.io:8080/>"
+ 
+     @user = user
+     @post = post
+       
+     mail(to: user.email, subject: "You have favorited and will recieve notifications from #{post.title}")
+ end
 end
