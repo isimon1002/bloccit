5a0c7d6d739a43a73817a769e1712079e53da4af
Isaac Simon <ijsimon@sbcglobal.net>
2017-09-13 00:29:46 +0000

    check point 31 Assignment

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,13 @@
 module UsersHelper
+    def has_posts?(user)
+        user.posts.any?
+    end
+    
+    def has_comments?(user)
+        user.comments.any?
+    end
+    
+    def has_favorites?(user)
+        user.favorites.any?
+    end
 end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -11,5 +11,26 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe UsersHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
+   let(:my_user) { create(:user) }
+   let(:my_topic) { create(:topic) }
+   let(:my_post) { create(:post, topic: my_topic, user: my_user) }
+   let(:my_comment) { create(:comment, post: my_post, user: my_user) }
+    describe "has_posts(user)" do
+      it "checks to see if a user has posts" do
+          po = my_user.posts.where(user: my_user).create
+          expect(helper.has_posts?(my_user)).to eq(true)
+      end
+  end
+     describe "has_comments(user)" do
+       it "checks to see if a user has posts" do
+          comment = my_user.comments.where(post: my_post, user: my_user).create
+          expect(helper.has_comments?(my_user)).to eq(true)
+      end
+  end
+     describe "has_favorites(user)" do
+       it "checks to see if a user has favorites" do
+          favorite = my_user.favorites.where(post: my_post, user: my_user).create
+          expect(helper.has_favorites?(my_user)).to eq(true)
+      end
+  end
 end
